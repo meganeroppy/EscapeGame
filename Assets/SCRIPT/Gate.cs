957c8a9948f515b2372556f89d21e641efb42213
m.roppy <roppy@hotmail.co.jp>
2015-10-31 16:48:26 +0900

    level1のチェンバーを改修

---
+++ Assets/SCRIPT/Gate.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class Gate : MonoBehaviour {
+	
+	[SerializeField]
+	private Vector3 unlockedOffset;
+	
+	private bool unlockd = false;
+	
+	[SerializeField]
+	private GateKey keyObject;
+	
+	private void Awake(){
+		transform.localPosition = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(keyObject == null){
+			Debug.Log( "A keyObject has to be assigned!");
+			return;
+		}
+		
+		if(!unlockd && keyObject.unlocked){
+			unlockd = true;
+			Unlock();
+		}
+	}
+	
+	private void Unlock(){
+		Debug.Log("Gate Unlocked");
+		transform.DOLocalMove(transform.localPosition + unlockedOffset, 1f);
+	}
+}
---
+++ Assets/SCRIPT/GateKey.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GateKey : MonoBehaviour {
+
+	public bool unlocked{get;private set;}
+
+	private void Awake(){
+		unlocked = false;
+	}
+
+	private void OnCollisionEnter(Collision col){
+		if(col.gameObject.tag.Equals("PlayerHands")){
+			if(!unlocked){
+				Debug.Log("GateKey Unlocked");
+				unlocked = true;
+			}
+		}
+	}
+}
