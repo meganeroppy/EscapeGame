fa333d783059bed4be8e0b734f5a1f85ca8c328f
m.roppy <roppy@hotmail.co.jp>
2015-11-02 13:03:07 +0900

    カメラモデルを追加

---
+++ Assets/SCRIPT/ChamberMaker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChamberMaker : MonoBehaviour {
+
+	private int line = 10;
+	private int raw = 10;
+	private int floor = 10;
+
+	[SerializeField]
+	GameObject chamber;
+	
+	private float offset = 50f;
+
+	// Use this for initialization
+	void Start () {
+		for(int l = 0 ; l < line ; l++){
+			for(int r = 0 ; r < raw ; r++){
+				for(int f=0 ; f < floor ; f++){
+					GameObject g = Instantiate(chamber);
+					g.transform.position = new Vector3(l * offset, r * offset, f * offset);
+					
+				}
+			}
+		}
+	}
+
+}
